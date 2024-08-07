b738ad36039ff632ccacdffed8ac76947725152c
Igor Mokhov <igormokhovid@gmail.com>
2024-08-07 12:23:52 +0300

    feat: added routing in the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './components/Layout/Layout';
+import { HomePage } from './pages/HomePage/HomePage';
+import { AccountPage } from './pages/AccountPage/AccountPage';
+import { TokenLaunchesPage } from './pages/TokenLaunchesPage/TokenLaunchesPage';
+import { TransactionsPage } from './pages/TransactionsPage/TransactionsPage';
+import { WalletPage } from './pages/WalletPage/WalletPage';
+import { VerificationPage } from './pages/VerificationPage/VerificationPage';
 import './App.scss';
 
 function App() {
-  return <div className="app">platforma</div>;
+  return (
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="account" element={<AccountPage />} />
+          <Route path="token-launches" element={<TokenLaunchesPage />} />
+          <Route path="transactions" element={<TransactionsPage />} />
+          <Route path="wallet" element={<WalletPage />} />
+          <Route path="verification" element={<VerificationPage />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
