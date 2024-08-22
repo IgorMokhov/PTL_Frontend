f8013c5a05ba64d9271c40e97a2c9c4dcb62775f
Igor Mokhov <igormokhovid@gmail.com>
2024-08-22 18:05:49 +0300

    feat: added TransactionItem

---
+++ src/components/TransactionItem/TransactionItem.tsx
@@ -0,0 +1,35 @@
+import styles from './TransactionItem.module.scss';
+
+interface TransactionItemProps {
+  tokenIcon: string;
+  token: string;
+  project: string;
+  cost: string;
+  purchased: string;
+  quantity: string;
+  total: string;
+}
+
+export const TransactionItem = ({
+  tokenIcon,
+  token,
+  project,
+  cost,
+  purchased,
+  quantity,
+  total,
+}: TransactionItemProps) => {
+  return (
+    <div className={styles.transaction_item}>
+      <span>
+        <img src={tokenIcon} alt="tokenIcon" />
+        {token}
+      </span>
+      <span>{project}</span>
+      <span>{cost}</span>
+      <span>{purchased}</span>
+      <span>{quantity}</span>
+      <span>{total}</span>
+    </div>
+  );
+};
