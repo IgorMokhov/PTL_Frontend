15d0ef0034702f39e78bd9bc58b9232f53a3bdd4
Igor Mokhov <igormokhovid@gmail.com>
2024-08-22 18:07:01 +0300

    feat: added TransactionList

---
+++ src/components/TransactionList/TransactionList.tsx
@@ -0,0 +1,35 @@
+import iconTest from '../../assets/icons/testIcon.svg';
+import { TransactionItem } from '../TransactionItem/TransactionItem';
+import styles from './TransactionList.module.scss';
+
+export const TransactionList = () => {
+  // test data
+  const transactions = [
+    {
+      tokenIcon: iconTest,
+      token: '$PUSH',
+      project: 'Apusher',
+      cost: '$150',
+      purchased: '$10B',
+      quantity: '10.0',
+      total: '$1,500',
+    },
+    {
+      tokenIcon: iconTest,
+      token: '$RDYX',
+      project: 'READYgg',
+      cost: '$0.16',
+      purchased: '$1.6B',
+      quantity: '200.0',
+      total: '$500',
+    },
+  ];
+
+  return (
+    <div className={styles.transaction_list}>
+      {transactions.map((transaction) => (
+        <TransactionItem {...transaction} />
+      ))}
+    </div>
+  );
+};
