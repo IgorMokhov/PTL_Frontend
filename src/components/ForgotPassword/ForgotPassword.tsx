81fe5a62135e95d33127d6e88d824aaf82e9bdc9
Igor Mokhov <igormokhovid@gmail.com>
2024-09-03 14:41:17 +0300

    feat: refactored ForgotPassword

--- src/components/ForgotPassword/ForgotPassword.tsx
+++ src/components/ForgotPassword/ForgotPassword.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { LoginHeader } from '../LoginHeader/LoginHeader';
 import { useNavigate } from 'react-router-dom';
@@ -5,17 +6,22 @@ import { CustomButton } from '../CustomButton/CustomButton';
 import styles from './ForgotPassword.module.scss';
 
 interface IFormInput {
+  email: string;
   password: string;
 }
 
 export const ForgotPassword = () => {
+  const [step, setStep] = useState(1);
   const navigate = useNavigate();
-  const { register, handleSubmit, reset } = useForm<IFormInput>();
 
-  const onSubmit: SubmitHandler<IFormInput> = (data) => {
-    console.log(data);
-    navigate('/login');
-    reset();
+  const { register, handleSubmit } = useForm<IFormInput>();
+
+  const onSubmit: SubmitHandler<IFormInput> = ({ email, password }) => {
+    if (step === 1) {
+      console.log(email);
+    } else if (step === 2) {
+      console.log(password);
+    }
   };
 
   return (
@@ -23,43 +29,84 @@ export const ForgotPassword = () => {
       <div className={styles.forgot_pass}>
         <LoginHeader />
 
-        <h6 className={styles.forgot_pass_subtitle}>Enter your new password</h6>
-        <p className={styles.forgot_pass_text}>
-          We have sent your new password to your email{' '}
-          <span>your.mail@gmail.com.</span>
-          Check your message and enter your new password to restoring access to
-          your account
-        </p>
-
-        <form
-          className={styles.forgot_pass_form}
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <label htmlFor="password">Enter password:</label>
-          <input
-            {...register('password')}
-            type="password"
-            placeholder="Password from the letter"
-          />
-          <div className={styles.forgot_pass_btns}>
-            <CustomButton
-              width={140}
-              height={53}
-              onClick={() => navigate('/login')}
+        {step == 1 ? (
+          <>
+            {' '}
+            <h6 className={styles.forgot_pass_subtitle}>Enter your email</h6>
+            <p className={styles.forgot_pass_text}>
+              Enter the email address you used to register your account. We will
+              send you a new password in a letter.
+            </p>
+            <form
+              className={styles.forgot_pass_form}
+              onSubmit={handleSubmit(onSubmit)}
             >
-              Back
-            </CustomButton>
-            <CustomButton
-              width={247}
-              height={53}
-              variant={'inverted'}
-              type={'submit'}
-              onClick={() => navigate('/login')}
+              <label htmlFor="email">Email:</label>
+              <input
+                {...register('email')}
+                type="email"
+                placeholder="Your email address"
+              />
+              <div className={styles.forgot_pass_btns}>
+                <CustomButton
+                  width={140}
+                  height={53}
+                  onClick={() => navigate('/login')}
+                >
+                  Back
+                </CustomButton>
+                <CustomButton
+                  width={226}
+                  height={53}
+                  variant={'inverted'}
+                  type={'submit'}
+                >
+                  Send password
+                </CustomButton>
+              </div>
+            </form>
+          </>
+        ) : (
+          <>
+            <h6 className={styles.forgot_pass_subtitle}>
+              Enter your new password
+            </h6>
+            <p className={styles.forgot_pass_text}>
+              We have sent your new password to your email{' '}
+              <span>your.mail@gmail.com.</span>
+              Check your message and enter your new password to restoring access
+              to your account
+            </p>
+            <form
+              className={styles.forgot_pass_form}
+              onSubmit={handleSubmit(onSubmit)}
             >
-              Change password
-            </CustomButton>
-          </div>
-        </form>
+              <label htmlFor="password">Enter password:</label>
+              <input
+                {...register('password')}
+                type="password"
+                placeholder="Password from the letter"
+              />
+              <div className={styles.forgot_pass_btns}>
+                <CustomButton
+                  width={140}
+                  height={53}
+                  onClick={() => setStep(1)}
+                >
+                  Back
+                </CustomButton>
+                <CustomButton
+                  width={247}
+                  height={53}
+                  variant={'inverted'}
+                  type={'submit'}
+                >
+                  Change password
+                </CustomButton>
+              </div>
+            </form>
+          </>
+        )}
       </div>
     </div>
   );
