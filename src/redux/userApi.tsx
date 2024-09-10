74465e40075d9445e02c88f351aa66df129c1989
Igor Mokhov <igormokhovid@gmail.com>
2024-09-10 18:00:33 +0300

    feat: added typing for signup in the userApi

--- src/redux/userApi.tsx
+++ src/redux/userApi.tsx
@@ -1,11 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { User } from '../types/user';
 
 export const userApi = createApi({
   reducerPath: 'userApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://31.128.42.224:8000' }),
 
   endpoints: (builder) => ({
-    signup: builder.mutation({
+    signup: builder.mutation<string, User>({
       query: (signupData) => ({
         url: '/auth/reg',
         method: 'POST',
