31d1306fbc230f508c222a4f6b311627b3b74f1d
Igor Mokhov <igormokhovid@gmail.com>
2024-09-17 12:30:43 +0300

    fix: added export for interface UpdatedPassword

--- src/redux/userApi.tsx
+++ src/redux/userApi.tsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { Login, LoginResponse, User } from '../types/user';
 import { RootState } from './store';
 
-interface UpdatedPassword {
+export interface UpdatedPassword {
   confirm_password: string;
   new_password: string;
   old_password: string;
